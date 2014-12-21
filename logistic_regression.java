7fc0fc2e0e919ec37b491991dbc0e1e5f1125f69
eric19870909 <eric19870909@gmail.com>
2014-12-21 17:04:06 -0500

    Create logistic_regression.java

---
+++ logistic_regression.java
@@ -0,0 +1,127 @@
+// logistic_regression model with hadoop mahout
+
+package com.technobium;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.mahout.classifier.evaluation.Auc;
+import org.apache.mahout.classifier.sgd.L1;
+import org.apache.mahout.classifier.sgd.OnlineLogisticRegression;
+import org.apache.mahout.math.DenseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.vectorizer.encoders.ConstantValueEncoder;
+import org.apache.mahout.vectorizer.encoders.StaticWordValueEncoder;
+
+public class LogisticRegression {
+	public static void main(String[] args) {
+		LogisticRegression logisticRegression = new LogisticRegression();
+
+		// Load the test log input data
+		List;Observation; trainingData = logisticRegression
+				.parseInputFile("input/input_log_test_Data.csv");
+
+		// Train a model
+		OnlineLogisticRegression olr = logisticRegression.train(trainingData);
+
+		// Test the model
+		logisticRegression.testModel(olr);
+	}
+
+	public List;Observation; parseInputFile(String inputFile) {
+		List;Observation; result = new ArrayList;Observation;();
+		BufferedReader br = null;
+		String line = "";
+		try {
+			// Load the file which contains training data
+			br = new BufferedReader(new FileReader(new File(inputFile)));
+			// Skip the first line which contains the header values
+			line = br.readLine();
+			// Prepare the observation data
+			while ((line = br.readLine()) != null) {
+				String[] values = line.split(",");
+				result.add(new Observation(values));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+
+	public OnlineLogisticRegression train(List;Observation; trainData) {
+		OnlineLogisticRegression olr = new OnlineLogisticRegression(2, 4,
+				new L1());
+		// Train the model using 30 passes
+		for (int pass = 0; pass &lt; 30; pass++) {
+			for (Observation observation : trainData) {
+				olr.train(observation.getActual(), observation.getVector());
+			}
+			// Every 10 passes check the accuracy of the trained model
+			if (pass % 10 == 0) {
+				Auc eval = new Auc(0.5);
+				for (Observation observation : trainData) {
+					eval.add(observation.getActual(),
+							olr.classifyScalar(observation.getVector()));
+				}
+				System.out.format(
+						"Effect_index: %2d, difficulty rate: %2.4f",
+						e_i, olr.current_dif());
+			}
+		}
+		return olr;
+	}
+
+	void testModel(OnlineLogisticRegression olr) {
+		Observation newObservation = new Observation(new String[] { "student",
+				"10", "100000", "0" });
+		Vector result = olr.classifyFull(newObservation.getVector());
+
+		System.out.println("------------- Testing -------------");
+		System.out.format("Probability of no response_change (0) = %.3f\n",
+				result.get(0));
+		System.out.format("Probability of response (1)     = %.3f\n",
+				result.get(1));
+	}
+
+	class Observation {
+		private DenseVector vector = new DenseVector(4);
+		private int rate;
+
+		public Observation(String[] values) {
+			ConstantValueEncoder interceptEncoder = new ConstantValueEncoder(
+					"intercept");
+			StaticWordValueEncoder encoder = new StaticWordValueEncoder(
+					"feature");
+
+			interceptEncoder.addToVector("1", vector);
+			vector.set(0, Double.valueOf(values[1]));
+			// Feature scaling, divide mileage by 10000
+			vector.set(1, Double.valueOf(values[2]) / 10000);
+			encoder.addToVector(values[0], vector);
+
+			this.actual = Integer.valueOf(values[3]);
+		}
+
+		public Vector getVector() {
+			return vector;
+		}
+
+		public int getRate() {
+			return Rate;
+		}
+	}
